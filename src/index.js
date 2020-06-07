3ca096f2a1415a2dfa2330525802927b1833069f
amfosh <afasciocco@gmail.com>
2020-06-07 14:49:51 -0400

    in order

--- src/index.js
+++ src/index.js
@@ -10,23 +10,32 @@ const colors =
 {"hex": "#dd4124", "name": "Tangerine Tango", "year": "2012"},
 {"hex": "#d85a7b", "name": "Honeysuckle", "year": "2011"},
 {"hex": "#53b0ae", "name": "Turquoise", "year": "2010"},
-// {"hex": "#55865C", "name": "17-5641", "year": "2009"},
-// {"hex": "#55865C", "name": "17-5641", "year": "2008"},
-// {"hex": "#55865C", "name": "17-5641", "year": "2007"},
-// {"hex": "#55865C", "name": "17-5641", "year": "2006"},
+{"hex": "#f0c05a", "name": "Mimosa", "year": "2009"},
+{"hex": "#6364a5", "name": "Blue Iris", "year": "2008"},
+{"hex": "#ca7c74", "name": "Chili Pepper", "year": "2007"},
+{"hex": "#decebb", "name": "Sand Dollar", "year": "2006"},
+{"hex": "#53b0ae", "name": "Blue Turquoise", "year": "2005"},
+{"hex": "#e2583e", "name": "Tigerlily", "year": "2004"},
+{"hex": "#7bc4c4", "name": "Aqua Sky", "year": "2003"},
+{"hex": "#bf1932", "name": "True Red", "year": "2002"},
+{"hex": "#c74375", "name": "Fuchsia Rose", "year": "2001"},
+{"hex": "#9bb7d4", "name": "Cerulean", "year": "2000"},
 ]
 
     const color = document.getElementById('color');
     const colorName = document.getElementById('color-name');
     const colorClick = document.getElementById('color-click');
     const colorYear = document.getElementById('color-year');
+    let count = 0;
 
 function colorChange() {
     colorClick.addEventListener("click", function() {
-    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    let randomColor = colors[count];
+    count ++
+    // [Math.floor(Math.random() * colors.length)];
     color.style.backgroundColor = randomColor["hex"];
     colorName.innerText = randomColor["name"];
     colorYear.innerText = randomColor["year"];
     })
 }
-colorChange();
+colorChange()
